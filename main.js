b7db1cd9361402b98b125003fa81b1d6aa89a4c0
maritaalva <malva3239@gmail.com>
2022-02-15 13:28:02 +0100

    prueba

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let nombre = prompt("¿cómo te llamas?");
+let apellidos = prompt("¿y cómo te apellidas?");
+let direccion = prompt("¿dónde vives (calle/número/código postal)?");
+let edad= prompt("¿Cuantos años tienes?");
+let correo = prompt("¿cómno es tu correo electrónico?");
+
+alert("Ya estas registrado para el juegon del calamar, tus datos son: " + nombre + " " + apellidos + " - " + direccion +" - " + edad + " - " + correo)
